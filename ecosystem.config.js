94e8eba28e79597a3572155772fc46e9e340c49a
anttonijulio <anttonijulio76@gmail.com>
2026-01-28 15:23:54 +0700

    add server and env to ecosystem js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: "web_fulusme",
+      name: "web-fulusme",
       script: "npm",
       args: "run start",
       env: {
         NODE_ENV: "production",
+        PORT: 3336,
+      },
+    },
+    {
+      name: "web-fulusme-staging",
+      script: "npm",
+      args: "run start",
+      env: {
+        NODE_ENV: "development",
         PORT: 3336,
       },
     },
