b1ee285bb67f8f0d4b1e53b9e17685fa7e2a0aea
anttonijulio <anttonijulio76@gmail.com>
2026-04-07 14:00:36 +0700

    migrate api base url

--- app/lib/price.ts
+++ app/lib/price.ts
@@ -1,29 +1,33 @@
 export function formatPriceOrEmpty(
   value: number | string | null | undefined,
-  locale = 'id-ID',
-  currency = 'IDR'
+  locale = "id-ID",
+  currency = "IDR",
 ): string {
-  const number = typeof value === 'string' ? parseFloat(value) : value ?? 0;
+  const number = typeof value === "string" ? parseFloat(value) : (value ?? 0);
 
-  if (!number || number === 0) return '';
+  if (!number || number === 0) return "";
 
   return priceLib(number.toString(), locale, currency);
 }
 
-export const priceLib = (value: string, locale: string = 'id-ID', currency: string = 'IDR'): string => {
-    const number = parseFloat(value);
+export const priceLib = (
+  value: string,
+  locale: string = "id-ID",
+  currency: string = "IDR",
+): string => {
+  const number = parseFloat(value);
   if (isNaN(number)) {
-    throw new Error('Invalid number input');
+    throw new Error("Invalid number input");
   }
 
   // Format the number without fractional digits
   const formatted = new Intl.NumberFormat(locale, {
-    style: 'currency',
+    style: "currency",
     currency: currency,
     minimumFractionDigits: 0, // No decimals
     maximumFractionDigits: 0, // No decimals
   }).format(number);
 
   // Return formatted value with commas removed
-  return formatted.replace(/,/g, '');
-}
+  return formatted.replace(/,/g, "");
+};
